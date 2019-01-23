f95a8726b6fce9d3adf4c04dfb13a149365b1229
Zergdeplowg <josterhus@comcast.net>
2019-01-23 17:07:18 -0600

    Untested initial code.

--- LaserTagGun/LaserTagGun.cpp
+++ LaserTagGun/LaserTagGun.cpp
@@ -7,4 +7,266 @@
 
 #include "LaserTagGun.h"
 
+LaserTagGun::LaserTagGun(int id)
+{
+	this->id = id;
+}
 
+void LaserTagGun::setupData()
+{
+	outputData = convertIntToBinary(damage);
+	dataLength = 8;
+	outputData = appendData(outputData, convertIntToBinary(team));
+	outputData = appendData(outputData, convertIntToBinary(id));
+}
+
+void LaserTagGun::setTriggerPin(int pin, bool checkState)
+{
+	triggerPin = pin;
+	triggerCheckState = checkState;
+}
+
+void LaserTagGun::setHitPin(int pin, bool checkState)
+{
+	hitPin = pin;
+	hitCheckState = checkState;
+}
+
+void LaserTagGun::setReloadPin(int pin, bool checkState)
+{
+	reloadPin = pin;
+	reloadCheckState = checkState;
+}
+
+void LaserTagGun::setIrOutPin(int pin)
+{
+	irOutPin = pin;
+}
+
+void LaserTagGun::update()
+{
+	if (digitalRead(hitPin) == hitCheckState)
+	{
+		inputData = Recive();
+		if (inputData != NULL)
+		{
+			handleDamage(convertToInt(inputData, 0, 8));
+			lastHit = millis();
+		}
+	}
+	if(digitalRead(triggerPin) == triggerCheckState & millis()-lastShot > 300 & !(ammo <= 0))
+	{
+		Fire();
+		lastShot = millis();
+		lastCharge = lastShot + (shieldRechargeDelay * 1000);
+		ammo--;
+	}
+	if(digitalRead(reloadPin) == reloadCheckState & millis() - lastReload > 300)
+	{
+		ammo = maxAmmo;
+		lastReload = millis();
+	}
+
+
+	if(millis()-lastHit > shieldRechargeDelay*1000)
+	{
+		if(shield < maxShield)
+		{
+			if (millis() - lastCharge > 0)
+			{
+				shield += (shieldRecharge / 1000)*(millis() - lastCharge);
+				lastCharge = millis();
+			}
+		}
+	}
+}
+
+void LaserTagGun::handleDamage(int damageRecived)
+{
+	shield -= damageRecived;
+	if (shield < 0)
+	{
+		health += shield;
+		shield = 0;
+	}
+}
+
+int * LaserTagGun::appendData(int * a, int * b)
+{
+	int* nData = new int[dataLength + 8];
+	for(int i = 0; i < dataLength; i ++)
+		nData[i] = a[i];
+	for (int i = 0; i < 8; i++)
+		nData[dataLength + i] = b[i];
+	dataLength += 8;
+	return nData;
+}
+
+void LaserTagGun::Fire()
+{
+	tone(irOutPin, IrFequency);
+	delayMicroseconds(IrPulse*2);
+	noTone(irOutPin);
+	delayMicroseconds(IrPulse*0.83);
+	for (int i = 0; i < dataLength; i++)
+	{
+		if(outputData[i] == 1)
+		{
+			tone(irOutPin, IrFequency);
+			delayMicroseconds(IrPulse*2);
+			noTone(irOutPin);
+		}
+		else
+		{
+			tone(irOutPin, IrFequency);
+			delayMicroseconds(IrPulse);
+			noTone(irOutPin);
+		}
+		delayMicroseconds(IrPulse*0.83);
+	}
+}
+
+int * LaserTagGun::Recive()
+{
+	int* data = new int[dataLength];
+	int t;
+	for (int i = 0; i < dataLength; i++)
+	{
+		t = pulseIn(hitPin, LOW);
+		if (t > 1000)
+			data[i] = 1;
+		else if (t > 0)
+			data[i] = 0;
+		else if (t == 0)
+		{
+			return NULL;
+		}
+	}
+	return data;
+}
+
+void LaserTagGun::setTeam(int teamId)
+{
+	team = teamId;
+}
+
+void LaserTagGun::setDamage(int damage)
+{
+	this->damage = damage;
+}
+
+void LaserTagGun::setHealth(float health)
+{
+	this->health = health;
+}
+
+void LaserTagGun::setMaxHealth(float maxHealth)
+{
+	this->maxHealth = maxHealth;
+}
+
+void LaserTagGun::setAmmo(int ammo)
+{
+	this->ammo = ammo;
+}
+
+void LaserTagGun::setMaxAmmo(int maxAmmo)
+{
+	this->maxAmmo = maxAmmo;
+}
+
+void LaserTagGun::setShield(float shield)
+{
+	this->shield = shield;
+}
+
+void LaserTagGun::setMaxShield(float maxShield)
+{
+	this->maxShield = maxShield;
+}
+
+void LaserTagGun::setShieldRecharge(float rate)
+{
+	shieldRecharge = rate;
+}
+
+void LaserTagGun::setShieldRechargeDelay(float delayT)
+{
+	shieldRechargeDelay = delayT;
+}
+
+int LaserTagGun::getTeam()
+{
+	return team;
+}
+
+int LaserTagGun::getDamage()
+{
+	return damage;
+}
+
+int LaserTagGun::getHealth()
+{
+	return health;
+}
+
+int LaserTagGun::getMaxHealth()
+{
+	return maxHealth;
+}
+
+int LaserTagGun::getAmmo()
+{
+	return ammo;
+}
+
+int LaserTagGun::getMaxAmmo()
+{
+	return maxAmmo;
+}
+
+int LaserTagGun::getShield()
+{
+	return shield;
+}
+
+int LaserTagGun::getMaxShield()
+{
+	return maxShield;
+}
+
+
+int * LaserTagGun::convertIntToBinary(int num)
+{
+	int *bList = new int[8];
+	for(int i = 0; i < 8; i++)
+	{
+		if (num % 2 == 0)
+			bList[i] = 1;
+		else
+			bList[i] = 0;
+		num = num / 2;
+		if (num == 0)
+			break;
+	}
+	int* output = new int[8];
+	for(int i = 0; i < 8; i++)
+	{
+		output[i] = bList[7 - i];
+	}
+	return output;
+}
+
+int LaserTagGun::convertToInt(int* data, int low, int high)
+{
+	int num = 0;
+	int multi = 64;
+	for (int i = low; i < high; i++)
+	{
+		if(data[i] == 1)
+		{
+			num += multi;
+		}
+		multi = multi / 2;
+	}
+}
--- LaserTagGun/LaserTagGun.h
+++ LaserTagGun/LaserTagGun.h
@@ -7,15 +7,86 @@
 #ifndef LaserTagGun_h
 #define LaserTagGun_h
 
-#include "arduino.h"
+#include "Arduino.h"
+//"C:\Program Files (x86)\Arduino\hardware\arduino\avr\cores\arduino\Arduino.h"
 
 class LaserTagGun
 {
 public:
-	LaserTagGun();
-	void Fire(bool *data, int size);
-	bool* Recive();
+
+	// must call these to set up your gun
+	LaserTagGun(int id);
+	void setupData(); // if using custom settings call after setting varables
+	void setTriggerPin(int pin, bool checkState);
+	void setHitPin(int pin, bool checkState);
+	void setReloadPin(int pin, bool checkState);
+	void setIrOutPin(int pin);
+
+
+	// run only if using default settings.
+	void update();
+
+	// default way of handleing damage
+	void handleDamage(int damageRecived);
+
+	// use when customizeing your gun
+	void Fire();
+	int* Recive();
+
+	// use to setup a custom gun
+	void setTeam(int teamId);
+	void setDamage(int damage);
+	void setHealth(float health);
+	void setMaxHealth(float maxHealth);
+	void setAmmo(int ammo);
+	void setMaxAmmo(int maxAmmo);
+	void setShield(float shield);
+	void setMaxShield(float maxShield);
+	void setShieldRecharge(float rate);
+	void setShieldRechargeDelay(float delayT);
+
+	int getTeam();
+	int getDamage();
+	int getHealth();
+	int getMaxHealth();
+	int getAmmo();
+	int getMaxAmmo();
+	int getShield();
+	int getMaxShield();
+
+	// handy functions for handleing data
+	int* convertIntToBinary(int num);
+	int convertToInt(int* data, int low, int high);
+	int* appendData(int* a, int* b);
+
 private:
+	int maxAmmo = 30;
+	int ammo = 30;
+	float maxShield = 100;
+	float shield = 100;
+	float shieldRecharge = 10;
+	float shieldRechargeDelay = 5;
+	float maxHealth = 100;
+	float health = 100;
+	bool triggerCheckState = LOW;
+	int triggerPin;
+	bool hitCheckState = LOW;
+	int hitPin;
+	bool reloadCheckState = LOW;
+	int reloadPin;
+	int irOutPin;
+	int dataLength;
+	int IrFequency = 57000;
+	int IrPulse = 600;
+	int damage = 25;
+	int id;
+	int team = 0;
+	int* inputData;
+	int* outputData;
+	int lastHit = 0;
+	int lastShot = 0;
+	int lastCharge = 0;
+	int lastReload = 0;
 
 
 
